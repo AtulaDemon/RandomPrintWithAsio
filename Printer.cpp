628a93924ef8174e674652eeeda804b7bde8daba
Atula Demon <caikinhdenlc@gmail.com>
2019-03-20 18:31:15 +0700

    Just 1/5 thread works

--- Printer.cpp
+++ Printer.cpp
@@ -15,12 +15,10 @@ void Printer::worksForThreads() {
 void Printer::printOneNumber() {
     global_stream_lock.lock();
 
-    std::random_device rd;
     std::mt19937_64 gen(rd());  //Predefined random number generators
     std::uniform_int_distribution<int> dis(1,1000); //Random number distributions
 
     std::cout << "Thread [" <<  std::this_thread::get_id()  << "] " << " print: " << dis(gen) <<  std::endl;
-    std::this_thread::sleep_for( std::chrono::milliseconds( 500 ) );
 
     global_stream_lock.unlock();
 }
@@ -28,6 +26,7 @@ void Printer::printOneNumber() {
 void Printer::worksOfService() {
     for(int i = 0; i <100; i++) {
         io_service -> post(std::bind(&printOneNumber, this));
+        std::this_thread::sleep_for(std::chrono::milliseconds( 500 ));
     }
 }
 
